f8c4a46eca9502b54045b1d77d6fe47fccf299a4
Sergey Bizin <biz-in@mail.ru>
2021-02-17 16:42:24 +0300

    first commit

---
+++ echobot.py
@@ -0,0 +1,12 @@
+import telebot
+
+bot = telebot.TeleBot('1622666315:AAEuQC1xYLhvwBVQh9lSdzKeCwLVlrpAflM')
+
+
+# Бот отвечает зеркально на любое сообщение.
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.send_message(message.chat.id, message.text)
+
+# Заставляет бота работать бесконечно.
+bot.polling(none_stop=True)
