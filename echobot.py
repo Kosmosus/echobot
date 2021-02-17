ec82ff8f9f6914d583e8af10ec7410c7752e0f0c
Sergey Bizin <biz-in@mail.ru>
2021-02-17 17:12:13 +0300

    second commit

--- echobot.py
+++ echobot.py
@@ -3,7 +3,7 @@ import telebot
 bot = telebot.TeleBot('1622666315:AAEuQC1xYLhvwBVQh9lSdzKeCwLVlrpAflM')
 
 
-# Бот отвечает зеркально на любое сообщение.
+# Бот отвечает зеркально.
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     bot.send_message(message.chat.id, message.text)
